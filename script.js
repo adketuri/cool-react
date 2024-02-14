8c09593230cb8f1dbb6f1ddf5ec12be1db05839a
Andrew Keturi <andrewketuri@gmail.com>
2024-02-13 18:08:42 -0800

    Split rendering work into fibers

--- script.js
+++ script.js
@@ -22,21 +22,84 @@ function createTextElement(text) {
   };
 }
 
+let nextUnitOfWork = null;
+
 function render(element, container) {
-  // recursively render the element and its children
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+function createDom(fiber) {
+  // recursively render the fiber and its children
   const dom =
-    element.type === "TEXT_ELEMENT"
+    fiber.type === "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   // assign the element props to our node
   const isProp = (key) => key !== "children";
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isProp)
-    .forEach((name) => (dom[name] = element.props[name]));
+    .forEach((name) => (dom[name] = fiber.props[name]));
+  return dom;
+}
 
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop); // setTimeout-like, runs when main is idle
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  // for each child elem, create a fiber
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    // add to the fiber tree, setting it as either a child or sibling
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // search for the next unit of work
+  if (fiber.child) return fiber.child;
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling;
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const CoolReact = {
@@ -53,8 +116,9 @@ const CoolReact = {
 /* @jsx CoolReact.createElement */
 const element = (
   <div id="foo">
-    <h1>hello h1</h1>
-    <p id="p">imma p</p>
+    <h1>very important website</h1>
+    <p id="p">I get to see my daddy on july 14th</p>
+    <img src="public/cameron-poe.gif" />
   </div>
 );
 
